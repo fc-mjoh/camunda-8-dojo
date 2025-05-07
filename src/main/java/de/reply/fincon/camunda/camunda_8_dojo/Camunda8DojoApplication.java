f31f600e508b5648370a50d73367b1adc56bd6d7
m.johannsen <markus.johannsen@reply.de>
2025-05-07 11:00:49 +0200

    first commit

---
+++ src/main/java/de/reply/fincon/camunda/camunda_8_dojo/Camunda8DojoApplication.java
@@ -0,0 +1,13 @@
+package de.reply.fincon.camunda.camunda_8_dojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Camunda8DojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Camunda8DojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/reply/fincon/camunda/camunda_8_dojo/Camunda8DojoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.reply.fincon.camunda.camunda_8_dojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Camunda8DojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
